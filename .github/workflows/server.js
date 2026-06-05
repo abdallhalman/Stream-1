dfdcf82e3c110332595b845cdfe4a6a1c4a76987
abdallhalman <abdallhalman@gmail.com>
2026-06-05 23:29:31 +0300

    Update server.js

--- .github/workflows/server.js
+++ .github/workflows/server.js
@@ -81,7 +81,7 @@ async function startOverlayStream() {
             console.error("Error in capture loop:", err.message);
         }
         // الاستمرار في التقاط الفريم التالي بناءً على السرعة المتاحة للمتصفح
-        setTimeout(captureLoop, 1000 / 15);
+        setTimeout(captureLoop, 1000 / );
     }
 
     // تشغيل حلقة الالتقاط لتجهيز الفريمات فوراً
