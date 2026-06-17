d8a45aa25bc1d49ea9595379f5b99d639b40d985
abdallhalman <abdallhalman@gmail.com>
2026-06-18 01:51:56 +0300

    Update server.js

--- .github/workflows/server.js
+++ .github/workflows/server.js
@@ -5,7 +5,7 @@ const WebSocket = require("ws");
 const path = require("path");
 const fs = require("fs");
 
-const TIKTOK_USER = "sal_u2";
+const TIKTOK_USER = "muhammad.aljaafar6";
 const STREAM_KEY = process.env.STREAM_KEY;
 const WIDTH  = 1280;
 const HEIGHT = 720;
